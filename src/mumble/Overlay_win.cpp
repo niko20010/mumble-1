mumble-voip/mumble

Move Windows overlay setup and injection to mumble_ol.exe.
This changes Mumble on Windows to run a helper process,
mumble_ol.exe, instead of loading mumble_ol.dll itself.

Prior to this change, Mumble would load mumble_ol.dll and
call PrepareD3D9() and PrepareDXGI() to set up the overlay.
Then, if the overlay was enabled, it would call InstallHooks()
to enable automatic injection of the overlay into new processes.
Similarly, it would call RemoveHooks() to disable automatic
overlay injection when the overlay was disabled in Mmuble.

With this change, Mumble instead runs a helper process called
mumble_ol.exe. This process sets up the overlay (equivalent
to calling PrepareD3D9(), PrepareDXGI() and calling InstallHooks()).
While running, it'll automatically inject the overlay into new
processes, just like Mumble itself did previously. On normal exit,
the helper process calls RemoveHooks() to ensure automatic overlay
injection is disabled.

To enable the overlay, Mumble starts the helper process.
To disable the overlay, Mumble terminates the helper process.
If the helper process dies when it is supposed to be running,
Mumble restarts it to ensure that overlay injection keeps
working as intended.

This change is the first part of enabling both an x86 and an x64
overlay to be active in Mumble at the same time. Since we cannot
load a 32-bit DLL into a 64-bit process (or vice versa), we need
a helper process for each architecture to reach our goal.
Note however that this commit in itself does not make it possible
for Mumble to run both an x86 and an x64 overlay at the same time.
This will come later.

src/mumble/Overlay_win.cpp
@@ -1,4 +1,5 @@
 /* Copyright (C) 2005-2011, Thorvald Natvig <thorvald@natvig.com>
+   Copyright (C) 2015, Mikkel Krautz <mikkel@krautz.dk>
 
    All rights reserved.
 
@@ -38,8 +39,7 @@
 #include "MainWindow.h"
 #include "Global.h"
 
-typedef unsigned int (__cdecl *GetOverlayMagicVersionProc)();
-typedef void (__cdecl *PrepProc)();
+#include "Overlay_win.h"
 
 // Used by the overlay to detect whether we injected into ourselves.
 //
@@ -49,52 +49,72 @@ typedef void (__cdecl *PrepProc)();
 extern "C" __declspec(dllexport) void mumbleSelfDetection() {};
 
 OverlayPrivateWin::OverlayPrivateWin(QObject *p) : OverlayPrivate(p) {
-	hpInstall = NULL;
-	hpRemove = NULL;
-	qlOverlay = new QLibrary(this);
+	m_helper_exe_path = QString::fromLatin1("%1/mumble_ol.exe").arg(qApp->applicationDirPath());
+	m_helper_exe_args = QStringList(QString::number(OVERLAY_MAGIC_NUMBER));
+	m_helper_process = new QProcess(this);
 
-	QString path = QString::fromLatin1("%1/mumble_ol.dll").arg(qApp->applicationDirPath());
-	qlOverlay->setFileName(path);
-	if (! qlOverlay->load()) {
-		QMessageBox::critical(NULL, QLatin1String("Mumble"), tr("Failed to load overlay library. This means either that:\n"
-		                      "- the library (mumble_ol.dll) wasn't found in the directory you ran Mumble from\n"
-		                      "- you're on an OS earlier than WinXP SP2"), QMessageBox::Ok, QMessageBox::NoButton);
-		qWarning("Overlay failure");
-		return;
-	}
+	connect(m_helper_process, SIGNAL(started()),
+	        this, SLOT(helperProcessStarted()));
 
-	GetOverlayMagicVersionProc gompvp = (GetOverlayMagicVersionProc)qlOverlay->resolve("GetOverlayMagicVersion");
-	if (! gompvp) {
-		qWarning("The overlay librarys overlay protocol version could not be verified. Overlay will not be enabled.");
-		return;
-	}
-
-	if (gompvp() != OVERLAY_MAGIC_NUMBER) {
-		qWarning("Client overlay protocol version does not match the overlay library one. Overlay will not be enabled.");
-		return;
-	}
-
-	hpInstall = (HooksProc)qlOverlay->resolve("InstallHooks");
-	hpRemove = (HooksProc)qlOverlay->resolve("RemoveHooks");
-	PrepProc prepareProc9 = (PrepProc) qlOverlay->resolve("PrepareD3D9");
-	PrepProc prepareProcDXGI = (PrepProc) qlOverlay->resolve("PrepareDXGI");
-
-	if (prepareProc9)
-		prepareProc9();
-
-	if (prepareProcDXGI)
-		prepareProcDXGI();
+	connect(m_helper_process, SIGNAL(finished(int, QProcess::ExitStatus)),
+	        this, SLOT(helperProcessExited(int, QProcess::ExitStatus)));
 }
 
 OverlayPrivateWin::~OverlayPrivateWin() {
-	qlOverlay->unload();
+	m_active = false;
 }
 
-void OverlayPrivateWin::setActive(bool act) {
-	if (act && hpInstall)
-		hpInstall();
-	else if (! act && hpRemove)
-		hpRemove();
+void OverlayPrivateWin::startHelper(QProcess *helper) {
+	if (helper->state() == QProcess::NotRunning) {
+		helper->start(m_helper_exe_path, m_helper_exe_args);
+	} else {
+		qWarning("OverlayPrivateWin: startHelper() called while process is already running. skipping.");
+	}
+}
+
+void OverlayPrivateWin::setActive(bool active) {
+	if (m_active != active) {
+		m_active = active;
+
+		if (m_active) {
+			startHelper(m_helper_process);
+		} else {
+			m_helper_process->terminate();
+		}
+	}
+}
+
+static const char *exitStatusString(QProcess::ExitStatus exitStatus) {
+	switch (exitStatus) {
+		case QProcess::NormalExit:
+			return "normal exit";
+		case QProcess::CrashExit:
+			return "crash";
+	}
+
+	return "unknown";
+}
+
+void OverlayPrivateWin::helperProcessStarted() {
+	QProcess *helper = qobject_cast<QProcess *>(sender());
+
+	PROCESS_INFORMATION *pi = helper->pid();
+	qWarning("OverlayPrivateWin: overlay helper process '%s' started with PID %llu.",
+	         qPrintable(m_helper_exe_path), static_cast<unsigned long long>(pi->dwProcessId));
+}
+
+void OverlayPrivateWin::helperProcessExited(int exitCode, QProcess::ExitStatus exitStatus) {
+	QProcess *helper = qobject_cast<QProcess *>(sender());
+
+	qWarning("OverlayPrivateWin: overlay helper process exited (%s) with status code %i.",
+	         exitStatusString(exitStatus),
+	         exitCode);
+
+	// If the helper process exited while we're in 'active'
+	// mode, restart it.
+	if (m_active) {
+			startHelper(helper);
+	}
 }
 
 void Overlay::platformInit() {

src/mumble/Overlay_win.h
@@ -1,0 +1,64 @@
+/* Copyright (C) 2005-2011, Thorvald Natvig <thorvald@natvig.com>
+   Copyright (C) 2015, Mikkel Krautz <mikkel@krautz.dk>
+
+   All rights reserved.
+
+   Redistribution and use in source and binary forms, with or without
+   modification, are permitted provided that the following conditions
+   are met:
+
+   - Redistributions of source code must retain the above copyright notice,
+     this list of conditions and the following disclaimer.
+   - Redistributions in binary form must reproduce the above copyright notice,
+     this list of conditions and the following disclaimer in the documentation
+     and/or other materials provided with the distribution.
+   - Neither the name of the Mumble Developers nor the names of its
+     contributors may be used to endorse or promote products derived from this
+     software without specific prior written permission.
+
+   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+   ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+   A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE FOUNDATION OR
+   CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+   EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+   PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+   PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+   LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+   NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+   SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#ifndef MUMBLE_MUMBLE_OVERLAY_WIN_H_
+#define MUMBLE_MUMBLE_OVERLAY_WIN_H_
+
+#include <QString>
+#include <QStringList>
+#include <QProcess>
+
+#include "Overlay.h"
+
+class OverlayPrivateWin : public OverlayPrivate {
+	private:
+		Q_OBJECT
+		Q_DISABLE_COPY(OverlayPrivateWin)
+
+	protected:
+		QProcess *m_helper_process;
+		QString m_helper_exe_path;
+		QStringList m_helper_exe_args;
+		bool m_active;
+
+		void startHelper(QProcess *helper);
+
+	public:
+		void setActive(bool);
+		OverlayPrivateWin(QObject *);
+		~OverlayPrivateWin();
+
+	public slots:
+		void helperProcessStarted();
+		void helperProcessExited(int exitCode, QProcess::ExitStatus exitStatus);
+};
+
+#endif

niko20010/mumble-1

src/mumble/Overlay_win.cpp
@@ -1,4 +1,5 @@
 /* Copyright (C) 2005-2011, Thorvald Natvig <thorvald@natvig.com>
+   Copyright (C) 2015, Mikkel Krautz <mikkel@krautz.dk>
 
    All rights reserved.
 
@@ -38,8 +39,9 @@
 #include "MainWindow.h"
 #include "Global.h"
 
-typedef unsigned int (__cdecl *GetOverlayMagicVersionProc)();
-typedef void (__cdecl *PrepProc)();
+#include "Overlay_win.h"
+
+#include "../../overlay/overlay_exe/overlay_exe.h"
 
 // Used by the overlay to detect whether we injected into ourselves.
 //
@@ -49,52 +51,73 @@ typedef void (__cdecl *PrepProc)();
 extern "C" __declspec(dllexport) void mumbleSelfDetection() {};
 
 OverlayPrivateWin::OverlayPrivateWin(QObject *p) : OverlayPrivate(p) {
-	hpInstall = NULL;
-	hpRemove = NULL;
-	qlOverlay = new QLibrary(this);
+	m_helper_exe_path = QString::fromLatin1("%1/mumble_ol.exe").arg(qApp->applicationDirPath());
+	m_helper_exe_args = QStringList(QString::number(OVERLAY_MAGIC_NUMBER));
+	m_helper_process = new QProcess(this);
 
-	QString path = QString::fromLatin1("%1/mumble_ol.dll").arg(qApp->applicationDirPath());
-	qlOverlay->setFileName(path);
-	if (! qlOverlay->load()) {
-		QMessageBox::critical(NULL, QLatin1String("Mumble"), tr("Failed to load overlay library. This means either that:\n"
-		                      "- the library (mumble_ol.dll) wasn't found in the directory you ran Mumble from\n"
-		                      "- you're on an OS earlier than WinXP SP2"), QMessageBox::Ok, QMessageBox::NoButton);
-		qWarning("Overlay failure");
-		return;
-	}
+	connect(m_helper_process, SIGNAL(started()),
+	        this, SLOT(onHelperProcessStarted()));
 
-	GetOverlayMagicVersionProc gompvp = (GetOverlayMagicVersionProc)qlOverlay->resolve("GetOverlayMagicVersion");
-	if (! gompvp) {
-		qWarning("The overlay librarys overlay protocol version could not be verified. Overlay will not be enabled.");
-		return;
-	}
-
-	if (gompvp() != OVERLAY_MAGIC_NUMBER) {
-		qWarning("Client overlay protocol version does not match the overlay library one. Overlay will not be enabled.");
-		return;
-	}
-
-	hpInstall = (HooksProc)qlOverlay->resolve("InstallHooks");
-	hpRemove = (HooksProc)qlOverlay->resolve("RemoveHooks");
-	PrepProc prepareProc9 = (PrepProc) qlOverlay->resolve("PrepareD3D9");
-	PrepProc prepareProcDXGI = (PrepProc) qlOverlay->resolve("PrepareDXGI");
-
-	if (prepareProc9)
-		prepareProc9();
-
-	if (prepareProcDXGI)
-		prepareProcDXGI();
+	connect(m_helper_process, SIGNAL(finished(int, QProcess::ExitStatus)),
+	        this, SLOT(onHelperProcessExited(int, QProcess::ExitStatus)));
 }
 
 OverlayPrivateWin::~OverlayPrivateWin() {
-	qlOverlay->unload();
+	m_active = false;
 }
 
-void OverlayPrivateWin::setActive(bool act) {
-	if (act && hpInstall)
-		hpInstall();
-	else if (! act && hpRemove)
-		hpRemove();
+void OverlayPrivateWin::startHelper(QProcess *helper) {
+	if (helper->state() == QProcess::NotRunning) {
+		helper->start(m_helper_exe_path, m_helper_exe_args);
+	} else {
+		qWarning("OverlayPrivateWin: startHelper() called while process is already running. skipping.");
+	}
+}
+
+void OverlayPrivateWin::setActive(bool active) {
+	if (m_active != active) {
+		m_active = active;
+
+		if (m_active) {
+			startHelper(m_helper_process);
+		} else {
+			m_helper_process->terminate();
+		}
+	}
+}
+
+static const char *exitStatusString(QProcess::ExitStatus exitStatus) {
+	switch (exitStatus) {
+		case QProcess::NormalExit:
+			return "normal exit";
+		case QProcess::CrashExit:
+			return "crash";
+	}
+
+	return "unknown";
+}
+
+void OverlayPrivateWin::onHelperProcessStarted() {
+	QProcess *helper = qobject_cast<QProcess *>(sender());
+
+	PROCESS_INFORMATION *pi = helper->pid();
+	qWarning("OverlayPrivateWin: overlay helper process '%s' started with PID %llu.",
+	         qPrintable(m_helper_exe_path), static_cast<unsigned long long>(pi->dwProcessId));
+}
+
+void OverlayPrivateWin::onHelperProcessExited(int exitCode, QProcess::ExitStatus exitStatus) {
+	QProcess *helper = qobject_cast<QProcess *>(sender());
+
+	const char *helperErrString = OverlayHelperErrorToString(static_cast<OverlayHelperError>(exitCode));
+	qWarning("OverlayPrivateWin: overlay helper process exited (%s) with status code %s.",
+	         exitStatusString(exitStatus),
+	         helperErrString ? helperErrString : qPrintable(QString::number(exitCode)));
+
+	// If the helper process exited while we're in 'active'
+	// mode, restart it.
+	if (m_active) {
+			startHelper(helper);
+	}
 }
 
 void Overlay::platformInit() {

src/mumble/Overlay_win.h
@@ -1,0 +1,64 @@
+/* Copyright (C) 2005-2011, Thorvald Natvig <thorvald@natvig.com>
+   Copyright (C) 2015, Mikkel Krautz <mikkel@krautz.dk>
+
+   All rights reserved.
+
+   Redistribution and use in source and binary forms, with or without
+   modification, are permitted provided that the following conditions
+   are met:
+
+   - Redistributions of source code must retain the above copyright notice,
+     this list of conditions and the following disclaimer.
+   - Redistributions in binary form must reproduce the above copyright notice,
+     this list of conditions and the following disclaimer in the documentation
+     and/or other materials provided with the distribution.
+   - Neither the name of the Mumble Developers nor the names of its
+     contributors may be used to endorse or promote products derived from this
+     software without specific prior written permission.
+
+   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+   ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+   A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE FOUNDATION OR
+   CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+   EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+   PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+   PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+   LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+   NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+   SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#ifndef MUMBLE_MUMBLE_OVERLAY_WIN_H_
+#define MUMBLE_MUMBLE_OVERLAY_WIN_H_
+
+#include <QString>
+#include <QStringList>
+#include <QProcess>
+
+#include "Overlay.h"
+
+class OverlayPrivateWin : public OverlayPrivate {
+	private:
+		Q_OBJECT
+		Q_DISABLE_COPY(OverlayPrivateWin)
+
+	public:
+		void setActive(bool);
+		OverlayPrivateWin(QObject *);
+		~OverlayPrivateWin();
+
+	public slots:
+		void onHelperProcessStarted();
+		void onHelperProcessExited(int exitCode, QProcess::ExitStatus exitStatus);
+
+	protected:
+		QProcess *m_helper_process;
+		QString m_helper_exe_path;
+		QStringList m_helper_exe_args;
+		bool m_active;
+
+		void startHelper(QProcess *helper);
+};
+
+#endif