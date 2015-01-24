b598df35210f895a1ef1850a9771ef33858eeb4c
Mikkel Krautz <mikkel@krautz.dk>
2015-01-24 11:01:31 +0100

    Add per-user volume adjustment.
    This adds an entry to the context menu of
    other clients, allowing users to manually
    adjust the volume of other users on the
    server.

--- src/mumble/MainWindow.cpp
+++ src/mumble/MainWindow.cpp
@@ -67,6 +67,7 @@
 #include "VoiceRecorderDialog.h"
 #include "../SignalCurry.h"
 #include "Settings.h"
+#include "VolumeSliderAction.h"
 
 #ifdef Q_OS_WIN
 #include "TaskList.h"
@@ -1231,6 +1232,12 @@ void MainWindow::qmUser_aboutToShow() {
 		qmUser->addSeparator();
 		qmUser->addAction(qaAudioMute);
 		qmUser->addAction(qaAudioDeaf);
+	}
+
+	if (p && !self) {
+		qmUser->addSeparator();
+		VolumeSliderAction *volumeSliderAction = new VolumeSliderAction(p->uiSession, this);
+		qmUser->addAction(volumeSliderAction);
 	}
 
 #ifndef Q_OS_MAC
